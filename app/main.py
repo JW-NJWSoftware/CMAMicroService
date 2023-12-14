4e4038c6e0cd699cd3dad010443d8b59b26919f9
jakepwheeler <jakephilipwheeler@gmail.com>
2023-12-14 22:16:25 +0000

    ai upgrades and multiple file formats handled

--- app/main.py
+++ app/main.py
@@ -2,14 +2,7 @@ import pathlib
 import os
 import io
 import uuid
-import PyPDF2
-import PyPDF2
-import nltk
-from transformers import pipeline
-from heapq import nlargest
-from nltk.tokenize import sent_tokenize
-from nltk.corpus import stopwords
-from nltk.probability import FreqDist
+import uvicorn
 from functools import lru_cache
 from fastapi import (
     FastAPI,
@@ -23,10 +16,9 @@ from fastapi import (
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic_settings import BaseSettings
+from .AIFunctions import extract_text_from_pdf, generate_text_summary, extract_names, extract_text_from_txt, extract_text_from_rtf
 
-# NLTK resources
-nltk.download('punkt')
-nltk.download('stopwords')
+baseAddress = ""
 
 class Settings(BaseSettings):
     debug: bool = False
@@ -61,64 +53,11 @@ def verify_auth(authorization = Header(None), settings:Settings = Depends(get_se
     if token != settings.app_auth_token:
         raise HTTPException(detail="Invalid authorization", status_code=401)
 
-def extract_text_from_pdf(file_path):
-    with open(file_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        num_pages = len(pdf_reader.pages)
-        text = ''
-        for page_num in range(num_pages):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
-    return text
-
-def generate_text_summary(text):
-    sentences = sent_tokenize(text)
-    num_sentences = int(len(sentences) * 0.75)
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-    # Preprocess the text
-    stopwords_list = set(stopwords.words('english'))
-    words = nltk.word_tokenize(text.lower())
-    words = [word for word in words if word.isalnum() and word not in stopwords_list]
-
-    # Calculate word frequency
-    frequency = FreqDist(words)
-
-    # Calculate sentence scores based on word frequency
-    sentence_scores = {}
-    for sentence in sentences:
-        for word in nltk.word_tokenize(sentence.lower()):
-            if word in frequency:
-                if len(sentence.split(' ')) < 30:  # Ignore very long sentences
-                    if sentence not in sentence_scores:
-                        sentence_scores[sentence] = frequency[word]
-                    else:
-                        sentence_scores[sentence] += frequency[word]
-
-    # Get the top 'num_sentences' sentences with highest scores
-    summary_sentences = nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
-    abstractedText = ' '.join(summary_sentences)
-
-      # Split the text into chunks (customize chunk size based on your requirements)
-    chunk_size = 1000
-    chunks = [abstractedText[i:i+chunk_size] for i in range(0, len(abstractedText), chunk_size)]
-
-    # Summarize each chunk and concatenate the summaries
-    summaries = []
-    for chunk in chunks:
-        summary = summarizer(chunk, max_length=150, min_length=30, do_sample=False)[0]['summary_text']
-        summaries.append(summary)
-
-    final_summary = " ".join(summaries)
-
-    return final_summary
-
-
-@app.get("/", response_class=HTMLResponse)
+@app.get(baseAddress + "/", response_class=HTMLResponse)
 def home_view(request: Request, settings:Settings = Depends(get_settings)):
     return templates.TemplateResponse("home.html", {"request": request})
 
-@app.post("/")
+@app.post(baseAddress + "/")
 async def file_analysis_view(file:UploadFile = File(...), authorization = Header(None), settings:Settings = Depends(get_settings)):
     verify_auth(authorization, settings)
     data = {}
@@ -134,32 +73,60 @@ async def file_analysis_view(file:UploadFile = File(...), authorization = Header
 
     file_extension = str(fname).split('.')[-1].lower()
 
-    if file_extension == 'txt':
-        data = {"filetype":"Plain text document"}
-    elif file_extension == 'doc' or file_extension == 'docx':
-        data = {"filetype":"Word document"}
-    elif file_extension == 'pdf':
-        text = extract_text_from_pdf(dest)
-        summary = generate_text_summary(text)
-        data = {
-            "filetype":"PDF document",
-            "summary":summary,
-            "text":text
-            }
-    elif file_extension == 'rtf':
-        data = {"filetype":"Rich Text Format (RTF) document"}
-    else:
-        data = {"filetype":"Unknown"}
+    try:
+        if file_extension == 'txt':
+            text = extract_text_from_txt(dest)
+            summary = generate_text_summary(text)
+            names = extract_names(text)
+            data = {
+                "filetype":"Plain text document",
+                "summary":summary,
+                "names":names,
+                "text":text
+                }
+        elif file_extension == 'doc' or file_extension == 'docx':
+            text = extract_text_from_doc(dest)
+            summary = generate_text_summary(text)
+            names = extract_names(text)
+            data = {
+                "filetype":"Word document",
+                "summary":summary,
+                "names":names,
+                "text":text
+                }
+        elif file_extension == 'pdf':
+            text = extract_text_from_pdf(dest)
+            summary = generate_text_summary(text)
+            names = extract_names(text)
+            data = {
+                "filetype":"PDF document",
+                "summary":summary,
+                "names":names,
+                "text":text
+                }
+        elif file_extension == 'rtf':
+            text = extract_text_from_rtf(dest)
+            summary = generate_text_summary(text)
+            names = extract_names(text)
+            data = {
+                "filetype":"Rich Text Format (RTF) document",
+                "summary":summary,
+                "names":names,
+                "text":text
+                }
+        else:
+            data = {"filetype":"Unknown"}
 
     # Delete the file from the uploads directory
-    try:
-        dest.unlink()  # Delete the file
-    except Exception as e:
-        print(f"Error deleting file: {e}")
+    finally:
+        try:
+            dest.unlink()  # Delete the file
+        except Exception as e:
+            print(f"Error deleting file: {e}")
 
     return data
 
-@app.post("/file-echo/", response_class=FileResponse)
+@app.post(baseAddress + "/file-echo/", response_class=FileResponse)
 async def file_upload(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
     if not settings.echo_active:
         raise HTTPException(detail="Invalid endpoint", status_code=400)
